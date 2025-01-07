85ac29e3626b445e8a17dfce2e332e6b960e4584
Helios360 <elias.treguier@gmail.com>
2025-01-07 15:42:32 +0100

    Done the addcard

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const addCardBtn = document.getElementById("addCardBtn");
   const searchInput = document.getElementById("searchInput");
   const sortByPriorityBtn = document.getElementById("sortByPriorityBtn");
-
+  let title;
+  let content;
+  let priority;
+  let index = 0;
+  const todo = document.getElementsById("todo");
   // Éventuellement, on écoute les événements
   addCardBtn.addEventListener("click", () => {
-    // ...
+    title = prompt("Titre*");
+    content = prompt("Contenu*");
+    priority = prompt("Priorité","low"); //"éventuellement une priorité"
+    todo.innerHTML+=```
+    <div class="card" data-id="${index++}" data-priority="${priotity}">
+      <h3>${title}</h3>
+      <p>${content}</p>
+    </div>  
+    ```;
   });
 
   searchInput.addEventListener("input", () => {
